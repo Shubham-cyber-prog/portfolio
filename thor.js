5fb049296c259492e5242cc103f3adceeb8a2347
Subham Nayak <sn343555@gmail.com>
2025-04-13 08:10:06 +0530

    Update thor.js

--- thor.js
+++ thor.js
@@ -1,3 +1,56 @@
+gsap.to(".box1",{
+    x:1500,
+    duration :1.5,
+    delay:1,
+    rotate:360,
+    borderRadius:"50%",
+    backgroundColor:"blue",
+    scale:0.5,
+    repeat:-1,
+    yoyo:true,
+})
+gsap.from(".box2",{
+    x:1000,
+    y:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    borderRadius:"50%",
+    backgroundColor:"blue",
+    repeat:-1,
+
+})
+gsap.to(".box3",{
+    x:1000,
+    y:500,
+    duration:3,
+    delay:1,
+    rotate:120,
+    borderRadius:"50%",
+    backgroundColor:"blue",
+    stagger:0.5,
+    repeat:-1,
+})
+gsap.from(".box4",{
+    x:1500,
+    duration:1.5,
+    delay:1,
+    rotate:360,
+    borderRadius:"50%",
+    backgroundColor:"goldenrod",
+    stagger:0.5,
+    repeat:-1,
+    yoyo:true,
+})
+gsap.from(".text-info",{
+    y:20,
+    opacity:0,
+    color:"blue",
+    duration:1,
+    delay:1,
+    stagger:0.5,
+    scale:0.5,
+})
 function myMenuFunction(){
     var menuBth = document .getElementById("myNavMenu");
 
